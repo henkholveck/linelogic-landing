ed217b0eda4f31234d36cd43793832447c73092c
henkholveck <henkmeyers@icloud.com>
2025-06-30 07:37:24 -0700

    Activate maintenance mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <MaintenanceCheck enabled={false} />
+        <MaintenanceCheck enabled={true} />
         <AuthProvider>
           {children}
         </AuthProvider>
